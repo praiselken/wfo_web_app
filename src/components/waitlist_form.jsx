5e75cd9e2e403e50a12b1fd8358ba93a86ff1570
pkprada <papraisel@gmail.com>
2025-01-04 01:20:16 +0000

    praisel code uodate

--- src/components/waitlist_form.jsx
+++ src/components/waitlist_form.jsx
@@ -136,7 +136,7 @@ export default function Waitlist() {
             <div>
               <label
                 htmlFor="dropdown"
-                className="block text-sm/6 font-medium text-white"
+                className="block text-sm/6 font-medium text-white mb-1"
               >
                 How did you find out about us?
               </label>
